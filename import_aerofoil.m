b53235aefe47b1366eea3a3563a3513acfe2f35c
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-02-23 15:15:11 +0000

    Update import_aerofoil.m
    fix variable names to avoid conflicts with inbuilt functions

--- import_aerofoil.m
+++ import_aerofoil.m
@@ -1,17 +1,17 @@
-function aerofoil = import_aerofoil(filename, plot)
+function aerofoil = import_aerofoil(filename, show)
 % IMPORT_AEROFOIL  get normalised x, y data from an aerofil
 %   aerofoil must be in a Selig format dat file - see airfoiltools.com
 %   output as a polyshape
 %   set plot as true to display imported geometry
 
     if nargin == 1
-        plot = false;
+        show = false;
     end
     
     imported = importdata(filename);
     aerofoil = polyshape(imported.data(:, 1), imported.data(:, 2));
     
-    if plot
+    if show
         
         figure
         plot(aerofoil);
