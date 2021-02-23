6ee55d24659d21a18362ef601ea31ea80bebb51d
shreybohra <42218014+shreybohra@users.noreply.github.com>
2021-02-23 14:53:39 +0000

    Update import_aerofoil.m
    Change behaviour to use polyshapes rather than coordinates

--- import_aerofoil.m
+++ import_aerofoil.m
@@ -1,8 +1,24 @@
-function [X, Y] = import_aerofoil(filename)
+function aerofoil = import_aerofoil(filename, plot)
 % IMPORT_AEROFOIL  get normalised x, y data from an aerofil
-%   aerfoil must be in the same format as obtained from airfoiltools.com
-%   output as two column vectors of X, Y coordinates
-    airfoil = importdata(filename);
-    X = airfoil.data(:, 1);
-    Y = airfoil.data(:, 2);
+%   aerofoil must be in the same format as obtained from airfoiltools.com
+%   output as a polyshape
+%   set plot as true to display imported geometry
+
+    if nargin == 1
+        plot = false;
+    end
+    
+    imported = importdata(filename);
+    aerofoil = polyshape(imported.data(:, 1), imported.data(:, 2));
+    
+    if plot
+        
+        figure
+        plot(aerofoil);
+        title ('Imported Geometry');
+        xlabel ('x');
+        ylabel ('y');
+        axis equal        
+    end
+        
 end
